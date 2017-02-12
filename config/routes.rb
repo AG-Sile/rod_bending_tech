e5c8a77e5ed8e8d81429452408a9915268a3fe04
Anna G <annaglusz@gmail.com>
2017-02-12 18:17:55 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/home',    to: 'static_pages#home'
+  get '/about',   to: 'static_pages#about'
+  get '/help',    to: 'static_pages#help'
+  get '/products', to: 'static_pages#products'
   root 'static_pages#home'
 end

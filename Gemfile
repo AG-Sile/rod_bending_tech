4d857dd7060e5c32d218cc2e372b1f1c5a8b5c81
Anna G <annaglusz@gmail.com>
2017-03-19 21:08:12 +0000

    adding sentry

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ end
 
 group :production do
   gem 'pg', '0.19'
+  gem "sentry-raven"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Rbt
   class Application < Rails::Application
+
+    Raven.configure do |config|
+      config.dsn = 'https://54d9c653382349b8a4fb2f8a56b30c55:a5555fabc8e5496eb1044e9678193123@sentry.io/149597'
+      config.filter_parameters << :password
+      config.environments = ['production']
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

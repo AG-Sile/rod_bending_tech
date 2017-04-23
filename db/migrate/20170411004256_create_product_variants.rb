3f3ddee5817ef05f06bc4863ee40ec6219de1cc3
Anna G <annaglusz@gmail.com>
2017-04-23 14:34:46 +0000

    fixing migrations

--- db/migrate/20170411004256_create_product_variants.rb
+++ db/migrate/20170411004256_create_product_variants.rb
@@ -1,8 +1,9 @@
 class CreateAttributes < ActiveRecord::Migration[5.0]
   def change
-    create_table :attributes do |t|
+    create_table :product_variant do |t|
       t.string :name
-      t.references :variant, foreign_key: true
+      t.string :variation
+      t.references :product, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20170415125242_changes_on_product_variants.rb
+++
@@ -1,6 +0,0 @@
-class ChangesOnProductVariants < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :variant_attributes
-    add_column :product_variants, :option, :string
-  end
-end
